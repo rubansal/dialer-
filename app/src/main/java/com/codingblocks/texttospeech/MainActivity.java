35445d257d4984d07a5f9512dcebc53ba23421da
rubansal <37189231+rubansal@users.noreply.github.com>
2020-09-05 17:07:55 +0530

    Update MainActivity.java

--- app/src/main/java/com/codingblocks/texttospeech/MainActivity.java
+++ app/src/main/java/com/codingblocks/texttospeech/MainActivity.java
@@ -1,70 +1,8 @@
-package com.codingblocks.texttospeech;
+import java.util.*;
 
-import android.speech.tts.TextToSpeech;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.util.Locale;
-
-public class MainActivity extends AppCompatActivity {
-
-    EditText editText;
-    Button btn;
-    TextToSpeech t;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        editText=findViewById(R.id.editText);
-        btn=findViewById(R.id.btn);
-
-        t=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status!=TextToSpeech.ERROR)
-                    t.setLanguage(Locale.US);
-            }
-        });
-
-//        editText.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                String toSpeak=s.toString();
-//                t.speak(toSpeak,TextToSpeech.QUEUE_FLUSH,null);
-//            }
-//        });
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String toSpeak=editText.getText().toString();
-                t.speak(toSpeak,TextToSpeech.QUEUE_FLUSH,null);
-            }
-        });
-    }
-
-    public void onPause() {
-        if(t!=null){
-            t.stop();
-            t.shutdown();
-        }
-        super.onPause();
+class JunkinTest{
+    
+    public static void main(String[] args){
+        System.out.println("test junkin git repo");
     }
 }
